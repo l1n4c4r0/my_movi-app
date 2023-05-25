87a88662b4b7a241f07b3e5f70eb316b8b99f74a
John9135 <palacioalexander5@gmail.com>
2023-05-25 12:43:33 -0500

    Create models genres and movie_genres

--- schemas/genres.py
+++ schemas/genres.py
@@ -4,3 +4,12 @@ from typing import Optional
 
 class Genres(BaseModel):
     id : Optional[int] = None
+    gen_title : str = Field(max_length=15,min_length=3, description="genero de la pelicula")
+
+    class Config: 
+        schema_extra = {
+            "example":{
+                "id":1,
+                "gen_title":"Action"
+            }
+        }
