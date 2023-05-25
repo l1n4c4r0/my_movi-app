8aab86adeec8c13d4f9b7fea312eb1140aa33255
Mafe <valenciamafe651@gmail.com>
2023-05-25 17:44:53 -0500

    create schema rating

---
+++ schemas/rating.py
@@ -0,0 +1,23 @@
+from pydantic import  BaseModel, Field
+from typing import Optional 
+
+
+class Rating(BaseModel):
+        id : Optional[int] = None
+        mov_id : int = Field(ge=1, description="foreing key of the movie ")
+        rev_id : int = Field(ge=1, description="foreing key of reviewer")
+        rev_stars_id : int = Field(ge=1, description="foreign key of the number of stars")
+        num_o_ratings_id : int = Field(ge=1, description="numbers and ratings")
+
+
+        class Config:
+            schema_extra = {
+                "example":{
+                    "mov_id":2,
+                    "rev_id":3,
+                    "rev_stars_":4,
+                    "num_o_ratings":5
+                }
+            }
+    
+    
