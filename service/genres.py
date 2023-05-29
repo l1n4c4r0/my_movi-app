9d9fdb540aec571d5bb6b65f235e8e0792f29bb9
lina <marcecaroc@gmail.com>
2023-05-29 16:41:01 -0500

    Create routers and servive of genres

--- service/genres.py
+++ service/genres.py
@@ -19,3 +19,14 @@ class GenresService():
     def get_for_id(self,id:int):
         result = self.db.query(GenresModel).filter(GenresModel.id == id).first()
         return result
+    
+    def update_genre(self,data:GenresModel):
+        genre = self.db.query(GenresModel).filter(GenresModel.id ==data.id).first()
+        genre.gen_title = data.gen_title
+        self.db.commit()
+        return
+
+    def delete_genre(self,id:int):
+        self.db.query(GenresModel).filter(GenresModel.id == id).delete()
+        self.db.commit()
+        return
