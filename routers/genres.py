6c3c760e7bb6b7204de8ce6f122a28d4c1dd2dff
John9135 <palacioalexander5@gmail.com>
2023-05-29 13:02:05 -0500

    genres get ger or id and post

--- routers/genres.py
+++ routers/genres.py
@@ -1,18 +1,34 @@
 from fastapi import APIRouter,Path,Query
 from fastapi.responses import HTMLResponse
 from fastapi.responses import  JSONResponse
+from fastapi.encoders import jsonable_encoder
+
+from service.genres import GenresService
+from schemas.genres import Genres 
+from config.database import Session
 
 
 genres_router = APIRouter()
 
-@genres_router.get('/genres_hello', tags=['genres'], status_code=200)
-def get_genres_hello():
-    return HTMLResponse('<h1>Hola desde la ruta de genres</h1>')
+#@genres_router.get('/genres_hello', tags=#['genres'], status_code=200)
+#def get_genres_hello():
+#    result = GenresService.get_genres_hello()
+#    return HTMLResponse(result)
 
 @genres_router.get('/genres', tags=['genres'], status_code=200)
 def get_genres():
-    return JSONResponse(content={'message':'this are generes'})
+    db = Session()
+    result = GenresService(db).get_genres()
+    return JSONResponse(content=jsonable_encoder(result), status_code = 200)
+
+@genres_router.get('/genres_for_id', tags=['genres'], status_code=200)
+def get_genre_for_id(id:int):
+    db = Session()
+    result = GenresService(db).get_for_id(id)
+    return JSONResponse(content=jsonable_encoder(result), status_code = 200)
 
 @genres_router.post('/genres', tags=['genres'], status_code=201)
-def create_genres():
-    return JSONResponse(content={"message":"genre created succesfully"})
+def create_genres(genres:Genres):
+    db = Session()
+    GenresService(db).create_genre(genres)
+    return JSONResponse(content={"message":"genre created succesfully", 'status_code':201})
